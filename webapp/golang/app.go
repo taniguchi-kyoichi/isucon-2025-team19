be394e93bfe2761ec79a4347d1ffa60034102ebb
yoshimoto_kotoha <kotoha-yoshimoto@dip-net.co.jp>
2025-06-11 14:51:58 +0900

    add: 静的ファイル配信の最適化

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -846,9 +846,5 @@ func main() {
 	r.Get("/admin/banned", getAdminBanned)
 	r.Post("/admin/banned", postAdminBanned)
 	r.Get(`/@{accountName:[a-zA-Z]+}`, getAccountName)
-	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir("../public")).ServeHTTP(w, r)
-	})
-
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
